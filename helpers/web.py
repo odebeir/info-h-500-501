bb6ea50a7b469c1320f77589378ef84be3e58ddb
Olivier Debeir <odebeir@ulb.ac.be>
2015-07-10 16:01:00 +0200

    add missing files

---
+++ helpers/web.py
@@ -0,0 +1,11 @@
+from IPython.display import HTML
+
+show_hide = '''<script>code_show=true; 
+    function code_toggle() {
+     if (code_show){$('div.input').hide();} else {$('div.input').show();}
+     code_show = !code_show}
+    $( document ).ready(code_toggle);</script>
+    <a href="javascript:code_toggle()">show/hide source code</a>
+    '''
+
+
