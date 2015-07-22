7b307facbe81006c1b547a9e8a368e2b0374894a
Olivier Debeir <odebeir@ulb.ac.be>
2015-07-22 15:45:20 +0200

    add equal

--- helpers/importer.py
+++ helpers/importer.py
@@ -9,15 +9,18 @@ def ext_image(name):
     """
     retrive an image from a url, save a local copy
     """
-    data_sources = {'mandrill':
-                'http://sipi.usc.edu/database/download.php?vol=misc&img=4.2.03'}
+    data_sources = {'mandrill.tif':
+                    'http://sipi.usc.edu/database/download.php?vol=misc&img=4.2.03',
+                    'bones.png':
+                    'http://homepages.ulb.ac.be/~odebeir/data/bones.png'}
     local_dir = 'data/'             
     assert(name in data_sources)
-    fname = os.path.join(local_dir,'%s.tif'%name)
+    fname = os.path.join(local_dir,'%s'%name)
     if os.path.exists(fname):
         return imread(fname)
     else:
         data = urlopen(data_sources[name]).read()
         open(fname,'wb').write(data)
         return imread(fname)
+
 
