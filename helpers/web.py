5f4998401c1b44a3701bb6d000281b44f5e86afe
odebeir <odebeir@ulb.ac.be>
2015-07-23 18:57:37 +0200

    adjust list format, switch to HTML

--- helpers/web.py
+++ helpers/web.py
@@ -28,6 +28,26 @@ def files_to_md(path='.',title='Chapter content',indent=0):
         md += '%s0. [%s](%s)\n'%(' '*indent,fn[3:-6],link)
     return md
 
+def files_to_html(path='.',title='Chapter content'):
+    """browses a given directory, returns a html formated string
+    use i.e. 
+    > from IPython.display import HTML
+    > HTML(files_to_html())
+    """
+    d = os.listdir(path)
+    chapters = sorted([s for s in d if ('00'<=s<='99')])
+    if title:
+        md = "<h1>\n%s\n</h1>\n"%(title)
+    else:
+        md = ''
+    files = sorted([s for s in d if ('00'<=s<='99') and s.endswith('.ipynb')])
+    md += "<ol>\n"
+    for fn in files:
+        link = '%s/%s'%(path,fn)
+        md += '<li><a href="%s">%s</a></li>'%(link,fn[3:-6])
+    md += "</ol>\n"
+    return md
+
 def chapters_to_md(path='.',title='Outline'):
     """browses a given directory, returns a markdown formated string
     of sbdirectories and files into them
@@ -43,3 +63,22 @@ def chapters_to_md(path='.',title='Outline'):
         md += '0. [%s](%s)\n'%(chap[3:],link)
         md += files_to_md(path=chap,title=None,indent=4)
     return md
+
+def chapters_to_html(path='.',title='Outline'):
+    """browses a given directory, returns a HTML formated string
+    of sbdirectories and files into them
+    use i.e. 
+    > from IPython.display import HTML
+    > HTML(chapters_to_html())
+    """
+    d = os.listdir(path)
+    chapters = sorted([s for s in d if ('00'<=s<='99')])
+    md = "<h1>\n%s\n</h1>\n"%(title)
+    md += "<ol start=0>\n"
+    for chap in chapters:
+        link = '%s/content.ipynb'%chap
+        md += '<li><a href="%s">%s</a></li>'%(link,chap[3:])
+        md += files_to_html(path=chap,title=None)
+    d += "</ol>\n"
+    return md
+    
